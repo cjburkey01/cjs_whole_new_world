87771a430483e62054eb4a076f1109836ea1ca91
CJ Burkey <cjburkey01@gmail.com>
2024-01-09 17:50:57 -0500

    Hello bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use bevy::log::{Level, LogPlugin};
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(LogPlugin {
+            filter: "wgpu=warn,naga=warn,bevy_render=info,bevy_app::plugin_group=info".to_string(),
+            level: Level::DEBUG,
+        }))
+        .run();
 }
